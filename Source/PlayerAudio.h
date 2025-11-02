295ffb9a781ffd5c1e997fc24f5564ac18ba88ea
soussanaishak6-source <soussanaishak6@gmail.com>
2025-11-02 21:15:26 +0200

    mute and unmute
    Added new methods for position and length retrieval.

--- Source/PlayerAudio.h
+++ Source/PlayerAudio.h
@@ -1,26 +1,33 @@
 #pragma once // PlayerAudio.h
 #include <JuceHeader.h>
+
 class PlayerAudio
 {
 public:
-	PlayerAudio();
-	~PlayerAudio();
-	void prepareToPlay(int samplesPerBlockExpected, double sampleRate);
-	void getNextAudioBlock(const juce::AudioSourceChannelInfo& bufferToFill);
-	void releaseResources();
-	bool loadFile(const juce::File&  file);
-	void play();
-	void stop();
-	bool isPlaying() const;
-	void setLooping(bool shouldLoop);
+    PlayerAudio();
+    ~PlayerAudio();
 
-	void setGain(float gain);
-	void setPosition(double pos);
-	double getPosition() const;
-	double getLength() const;
+    void prepareToPlay(int samplesPerBlockExpected, double sampleRate);
+    void getNextAudioBlock(const juce::AudioSourceChannelInfo& bufferToFill);
+    void releaseResources();
+    bool loadFile(const juce::File& file);
+    void play();
+    void stop();
+    bool isPlaying() const;
+    void setLooping(bool shouldLoop);
+
+    void setGain(float gain);
+    void setPosition(double pos);
+    double getPosition() const;
+    double getLength() const;
+
+    double getLengthInSeconds() const { return getLength(); }
+    double getCurrentPosition() const ; 
+    void setPositionRelative(float relativePosition);
+
 private:
-	juce::AudioFormatManager formatManager;
-	std::unique_ptr< juce::AudioFormatReaderSource> readerSource;
-	juce::AudioTransportSource transportSource;
-	// JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(PlayerAudio)
+    juce::AudioFormatManager formatManager;
+    std::unique_ptr<juce::AudioFormatReaderSource> readerSource;
+    juce::AudioTransportSource transportSource;
+    // JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(PlayerAudio)
 };
